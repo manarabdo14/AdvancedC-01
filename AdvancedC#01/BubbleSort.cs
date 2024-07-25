313587f727b31946ffbd938f0c82e6a459152251
Manar Abdo <mannarabdo14@gmail.com>
2024-07-25 21:53:25 +0300

    Add project files.

---
+++ AdvancedC#01/BubbleSort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedC_01
+{
+    internal class BubbleSort
+    {
+        public void Bubble_Sort(int[] array)
+        {
+            int n = array.Length;
+            bool swapped;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+
+                for (int j = 0; j < n - 1 - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        swapped = true;
+                    }
+
+                    if (!swapped)
+                    {
+                        break;
+                    }
+                }
+            }
+
+
+        }
+
+        public void PrintArray(int[] array)
+        {
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ AdvancedC#01/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace AdvancedC_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region 1 
+
+            //int[] array = { 64, 34, 25, 12, 22, 11, 90 };
+            //BubbleSort sorter = new BubbleSort();
+
+            //Console.WriteLine("Unsorted array:");
+            //sorter.PrintArray(array);
+
+            //sorter.Bubble_Sort(array);
+
+            //Console.WriteLine("Sorted array:");
+            //sorter.PrintArray(array);
+            #endregion
+
+
+            #region 2
+            //var intRange = new Range<int>(1, 10);
+            //Console.WriteLine("Is 5 in range: " + intRange.IsInRange(5)); // True
+            //Console.WriteLine("Is 0 in range: " + intRange.IsInRange(0)); // False
+            //Console.WriteLine("Length of range: " + intRange.Length());   // 9
+
+            //var doubleRange = new Range<double>(1.5, 9.3);
+            //Console.WriteLine("Is 5.5 in range: " + doubleRange.IsInRange(5.5)); // True
+            //Console.WriteLine("Is 1.0 in range: " + doubleRange.IsInRange(1.0)); // False
+            //Console.WriteLine("Length of range: " + doubleRange.Length());       // 7.8
+
+            //var dateRange = new Range<DateTime>(new DateTime(2020, 1, 1), new DateTime(2020, 12, 31));
+            //Console.WriteLine("Is 2020-06-15 in range: " + dateRange.IsInRange(new DateTime(2020, 6, 15))); // True
+            //Console.WriteLine("Is 2021-01-01 in range: " + dateRange.IsInRange(new DateTime(2021, 1, 1))); // False
+            //Console.WriteLine("Length of range: " + dateRange.Length().Day + " days"); // 365 days
+        //}
+        #endregion
+    }
+    }
+}
